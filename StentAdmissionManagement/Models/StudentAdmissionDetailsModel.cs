a0aeac4bb732d2eee19f35decf4265169978be36
Matias Sosa <msosa@hiberus.com>
2022-02-14 23:32:06 +0100

    Finished Test

---
+++ StentAdmissionManagement/Models/StudentAdmissionDetailsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StentAdmissionManagement.Models
+{
+    public class StudentAdmissionDetailsModel
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public string StudentClass { get; set; }
+        public DateTime DateofJoining { get; set; }
+    }
+}
